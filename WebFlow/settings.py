1ae3248ce2cd5b4c3c7c09998839d7180c5eef8d
pritish557 <pritishdash29@gmail.com>
2023-01-27 08:16:38 +0530

    updated

--- WebFlow/settings.py
+++ WebFlow/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-kd40a1fxq_0zkug=2t^9(syap)v^@ny^lzau%!k9w&h&l&o3te'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
